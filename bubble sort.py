e5ff1233c8bbaa4a95430b128a5eb6923f461a04
SanjibM99 <54479135+SanjibM99@users.noreply.github.com>
2020-06-28 17:39:14 +0530

    Add files via upload

---
+++ bubble sort.py
@@ -0,0 +1,11 @@
+def bubble(list):
+    for i in range(len(list)-1,0,-1):
+        for j in range(i):
+            if list[j]>list[j+1]:
+                list[j],list[j+1]=list[j+1],list[j]
+
+
+
+list=[23,66,1,33,2,565]
+bubble(list)
+print(list)
