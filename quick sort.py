e8e84908baca403db616cb5b660b973adcbca2ce
SanjibM99 <54479135+SanjibM99@users.noreply.github.com>
2020-06-28 17:34:35 +0530

    quicksort

---
+++ quick sort.py
@@ -0,0 +1,26 @@
+def part(list,first,last):
+	pivot=list[first]
+	left=first
+	right=last
+	while True:
+		while left<=right and list[left]<=pivot:
+			left+=1
+		while left<=right and list[right]>=pivot:
+			right-=1
+		if right<left:
+			break
+		else:
+			list[left],list[right]=list[right],list[left]
+	list[first],list[right]=list[right],list[first]
+	return right	
+	
+def quick(list,first,last):
+	if first<last:
+		p=part(list,first,last)
+		quick(list,first,p-1)
+		quick(list,p+1,last)
+
+list=[12,11,3,122,56,2,45]
+n=len(list)
+quick(list,0,n-1)
+print(list)
